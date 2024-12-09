0ea41c46d28787eda6f8638ada57276496d4b4eb
ermu <168235638@qq.com>
2024-12-09 12:59:34 +0800

    第二次提交

--- src/main.js
+++ src/main.js
@@ -6,4 +6,6 @@ import '@/assets/css/global.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
-createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
+import zhCn from "element-plus/es/locale/lang/zh-cn";
+
+createApp(App).use(store).use(router).use(ElementPlus, {locale:zhCn,}).mount('#app')
