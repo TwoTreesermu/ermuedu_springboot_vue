b4efe88974a59b386741024315e59dffc257a268
ermu <168235638@qq.com>
2024-12-09 16:43:21 +0800

    第三次提交

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,7 +11,7 @@
     </div>
 
     <el-table :data="tableData" stripe style="width: 100%">
-      <el-table-column prop="date" label="日期"></el-table-column>
+      <el-table-column sortable prop="date" label="日期"></el-table-column>
       <el-table-column prop="name" label="姓名"></el-table-column>
       <el-table-column prop="address" label="地址"></el-table-column>
       <el-table-column fixed="right" label="操作" width="100">
